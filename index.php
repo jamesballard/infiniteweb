fe748d2f8b7a6bfe9cead064fc914b8701c528d9
James Ballard <jimbonda@gmail.com>
2015-02-06 20:45:34 +1100

    Move Social Media icons to the 'Contact' section instead of footer

--- index.php
+++ index.php
@@ -465,14 +465,27 @@
             </div>
             <div class="row">
 				<div class="col-lg-12 text-center">
-					<p>Email: <a href="mailto:support@infiniterooms.co.uk">support@infiniterooms.co.uk</a></p>
+					<p><a href="mailto:support@infiniterooms.co.uk">support@infiniterooms.co.uk</a></p>
 				</div>
             </div>
-			<div class="row">
-				<div class="col-lg col-md-12 text-center">
-                    <p>
-                </p></div>
-			</div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <ul class="list-inline">
+                        <li><a href="https://www.infiniterooms.co.uk/#" class="btn-social"><i class="fa fa-2x fa-fw fa-facebook"></i></a>
+                        </li>
+                        <!--
+                        <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
+                        </li> -->
+                        <li><a href="https://twitter.com/infiniterooms" class="btn-social"><i class="fa fa-2x fa-fw fa-twitter"></i></a>
+                        </li>
+                        <li><a href="https://www.infiniterooms.co.uk/#" class="btn-social"><i class="fa fa-2x fa-fw fa-linkedin"></i></a>
+                        </li>
+                        <!--
+                        <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
+                        </li> -->
+                    </ul>
+                </div>
+            </div>
         </div>
     </section>
     <section class="success" id="about">
@@ -513,29 +526,6 @@
     </section>
 	
     <footer class="text-center">
-        <div class="footer-above">
-            <div class="container">
-                <div class="row">
-                    <div class="footer-col col-md-6">
-                        <h3>Social Media</h3>
-                        <ul class="list-inline">
-                            <li><a href="https://www.infiniterooms.co.uk/#" class="btn-social btn-outline"><i class="fa fa-fw fa-facebook"></i></a>
-                            </li>
-							<!--
-                            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-google-plus"></i></a>
-                            </li> -->
-                            <li><a href="https://twitter.com/infiniterooms" class="btn-social btn-outline"><i class="fa fa-fw fa-twitter"></i></a>
-                            </li>
-                            <li><a href="https://www.infiniterooms.co.uk/#" class="btn-social btn-outline"><i class="fa fa-fw fa-linkedin"></i></a>
-                            </li>
-							<!--
-                            <li><a href="#" class="btn-social btn-outline"><i class="fa fa-fw fa-dribbble"></i></a>
-                            </li> -->
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="footer-below">
             <div class="container">
                 <div class="row">
